a3aae2e00bc74bf4611423c022b6fd6b70c437d6
Sergio Pieshko <sergio.pieshko@icloud.com>
2022-03-30 11:48:40 +0300

    4

--- src/js/quiz app.js
+++ src/js/quiz app.js
@@ -41,45 +41,52 @@ const quizData = [
   },
 ];
 
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
 const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
-const submitButton = document.getElementById('submit');
+const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
-
 let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+  deselectAnswers();
+
   const currentQuizData = quizData[currentQuiz];
 
-  questionEl.innerHTML = currentQuizData.question;
-
+  questionEl.innerText = currentQuizData.question;
   a_text.innerText = currentQuizData.a;
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
-
-  currentQuiz++;
 }
-function getSelected() {
-  const answerEl = document.querySelectorAll('.answer');
 
+function getSelected() {
   let answer = undefined;
 
-  answers.forEach(answer => {
+  answerEls.forEach(answerEl => {
     if (answerEl.checked) {
       answer = answerEl.id;
     }
   });
+
   return answer;
 }
 
-submitButton.addEventListener('click', () => {
+function deselectAnswers() {
+  answerEls.forEach(answerEl => {
+    answerEl.checked = false;
+  });
+}
+
+submitBtn.addEventListener('click', () => {
+  // check to see the answer
   const answer = getSelected();
 
   if (answer) {
@@ -88,11 +95,14 @@ submitButton.addEventListener('click', () => {
     }
 
     currentQuiz++;
-
     if (currentQuiz < quizData.length) {
       loadQuiz();
     } else {
-      alert('You finished Quiz');
+      quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
     }
   }
 });
